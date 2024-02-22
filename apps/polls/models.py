bc903372f3bb7bbe5b5f2561507d73085bcbace2
seunghyeon kim <seunghyeon.me@gmail.com>
2024-02-22 15:59:36 +0000

    complete the dev of app: polls

--- apps/polls/models.py
+++ apps/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.CharField(max_length=200)
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.content
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content
